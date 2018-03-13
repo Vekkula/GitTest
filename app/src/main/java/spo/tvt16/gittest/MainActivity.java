be73b64723a929a4e291a9f495103557184cf609
Veli Jakkula <t5jave00@students.oamk.fi>
2018-03-13 11:40:47 +0200

    hello git

---
+++ app/src/main/java/spo/tvt16/gittest/MainActivity.java
@@ -0,0 +1,20 @@
+package spo.tvt16.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+
+        Log.d("msg","onClick");
+    }
+}
